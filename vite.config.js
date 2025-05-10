5f5ff23552ea8198dc7aeb8c3c40508ca2fa26f3
Arjun Rao <125185224+arjun1127@users.noreply.github.com>
2025-05-10 10:02:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// ⚠️ Replace with your repo name
 const repoName = 'potfolio-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, // 👈 important
+  base: `/${repoName}/`,
 });
